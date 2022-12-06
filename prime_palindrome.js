7450e74a67b5b2b461f1be7b0856bc5c1f466476
unknown <nikhildhanawade151098@gmail.com>
2022-12-06 16:47:19 +0530

    prime_palindrome

---
+++ prime_palindrome.js
@@ -0,0 +1,23 @@
+1.Check whether number is prime or not
+let num=5;
+let count=2;
+for(let i=0;i<=num;i++){
+
+if(num%i==2){
+count--
+console.log("prime number")
+}else{
+console.log("not an prime number")
+
+
+
+1.Check whether a string is palindrome or not
+let str="naman";
+let count=2;
+for(let i=str.length-1;i<=str.length;i--){
+
+if(str[i]==str.length)
+
+console.log(The string is palindrome")
+}else{
+console.log("The string is not palindrome")
