e0c3787f6d3e0105016874fb0eb471497c432a24
Deepak Nath <nathd3191@gmail.com>
2022-12-08 13:09:45 +0530

    updated

--- new.js
+++ new.js
@@ -1,3 +1,5 @@
+// 1. Prime Number
+
 var n = 6;
 var i;
 var flag = true;
@@ -12,3 +14,20 @@ if (flag) {
 } else {
     console.log("It is not Prime Number");
 }
+
+// 2. Paliondrome String
+let str = "MADAM";
+let flag = true;
+let len = str.length;
+for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+        flag = false;
+    }
+
+}
+if (flag) {
+    console.log("Palindrome");
+} else {
+    console.log("Not Palindrome");
+}
+
